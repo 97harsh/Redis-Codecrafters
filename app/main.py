b49e992fb900a6b6adb70fe00fbe2aa05ce6a12d
Harsh Sharma <harshsrharsh@gmail.com>
2024-04-12 18:40:15 -0400

    Task4, try:2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
+from _thread import start_new_thread
+print_lock = threading.Lock()
 
+def threaded(c):
+    # Function gets the connection, checks if it got a ping, returns a pong to it
+    # while True:
+    data = c.recv(1024)
+    if data==b"*1\r\n$4\r\nping\r\n":
+        c.send(b"+PONG\r\n")
+    else:
+        c.send(b"-Error message\r\n")
+    c.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,18 +20,26 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            # print(data)
-            # if b"ping\r\n" in data:
-            if data==b"*1\r\n$4\r\nping\r\n":
-                conn.send(b"+PONG\r\n")
-            else:
-                conn.send(b"-Error message\r\n")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("localhost", 6379))
+    sock.listen()
+    while True:
+        c, addr = sock.accept()
+        print_lock.acquire()
+        print(f"Connected by {addr[0]}")
+        start_new_thread(threaded, (c,))
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # conn, addr = server_socket.accept() # wait for client
+    # with conn:
+    #     print(f"Connected by {addr}")
+    #     while True:
+    #         data = conn.recv(1024)
+    #         # print(data)
+    #         # if b"ping\r\n" in data:
+    #         if data==b"*1\r\n$4\r\nping\r\n":
+    #             conn.send(b"+PONG\r\n")
+    #         else:
+    #             conn.send(b"-Error message\r\n")
     
 
 
