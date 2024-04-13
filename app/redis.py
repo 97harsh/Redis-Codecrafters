ff1e13c80da9e11f3127609bcd88215673cf9cab
Harsh Sharma <harshsrharsh@gmail.com>
2024-04-13 18:24:23 -0400

    added function to parse arguments

--- app/redis.py
+++ app/redis.py
@@ -1,13 +1,26 @@
-from time import time
+from time import time_ns
+from typing import Dict, List
 
 from app.parse import RESPParser
 from app.utils import current_milli_time
 
 class Redis:
+    ECHO = b"echo"
+    GET = b"get"
+    SET = b"set"
+    PING = b"ping"
+    PX = b"px"
+
+    LEN_ECHO = 2
+    LEN_GET = 2
+    LEN_SET = 3
+    LEN_PING = 1
+    LEN_PX = 2
+
     def __init__(self):
         self.memory = {}
         self.timeout = {} # Stores, current time, timeout in ms
-    
+
     def set_memory(self, key, value, **args):
         """
         Stores key value pair in memory 
@@ -29,7 +42,7 @@ class Redis:
 
         return self.memory.get(key,None)
 
-    def is_timeout(self, key):
+    def is_timeout(self, key: str) -> bool:
         if key not in self.timeout:
             # If key has no timeout mentioned
             return False
@@ -38,3 +51,29 @@ class Redis:
         if current_milli_time()-key_entered_time>key_life:
             return True
         return False
+    
+    def parse_arguments(self, input: List) -> Dict:
+        """
+        Expects a list of input which parses and coverts to a dictionary
+        """
+        curr = 0
+        result={}
+        while curr<len(input):
+            if input[curr].lower()==Redis.PING:
+                result[Redis.PING]=None
+                curr+=Redis.LEN_PING
+            elif input[curr].lower()==Redis.ECHO:
+                result[Redis.ECHO] = input[curr+1]
+                curr+=Redis.LEN_ECHO
+            elif input[curr].lower()==Redis.SET:
+                result[Redis.SET] = [input[curr+1], input[curr+2]]
+                curr+=Redis.LEN_SET
+            elif input[curr].lower()==Redis.GET:
+                result[Redis.GET] = input[curr+1]
+                curr+=Redis.LEN_GET
+            elif input[curr].lower()==Redis.PX:
+                result[Redis.PX] = input[curr+1]
+                curr+=Redis.LEN_PX
+            else:
+                raise ValueError(f"Unknown command {input[curr]}")
+        return result
